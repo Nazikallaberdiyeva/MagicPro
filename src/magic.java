7e700ef272efee0bf361e66761f032a8f6517868
Nazikallaberdiyeva <nazikallaberdiyeva@gmail.com>
2020-11-10 11:29:31 -0600

    adding

--- src/magic.java
+++ src/magic.java
@@ -2,5 +2,6 @@ public class magic {
     public static void main(String[] args) {
         System.out.println("Hello");
         // b21 is good at intellij
+        // mac user and windows users are
     }
 }
